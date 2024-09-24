23ac88df4148e79be5607fcd0cabf123a42ffbed
JustPowerful <ahmedaminedoudech@gmail.com>
2024-09-24 17:06:13 +0100

    refactor: add authOptions to a seperate file

--- apps/web/app/api/auth/[...nextauth]/route.ts
+++ apps/web/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,5 @@
-import { db, eq, users } from "@repo/database";
-import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { NextAuthOptions } from "next-auth";
-
+import { authOptions } from "@/utils/auth-options";
 declare module "next-auth" {
   interface Session {
     user: {
@@ -12,73 +8,6 @@ declare module "next-auth" {
     };
   }
 }
-
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/login",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-          placeholder: "johndoe@mail.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        if (!credentials?.email || !credentials?.password) {
-          return null;
-        }
-        const user = (
-          await db
-            .select()
-            .from(users)
-            .where(eq(users.email, credentials.email))
-        )[0];
-
-        if (!user) {
-          return null;
-        }
-
-        const correctPassword = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
-
-        if (!correctPassword) {
-          return null;
-        }
-        console.log(user.firstname, user.lastname);
-        return { ...user, id: user.id.toString() };
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.sub = user.id;
-        token.email = user.email;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user!.email = token.email;
-        session.user!.id = token.sub;
-      }
-      return session;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ apps/web/src/utils/auth-options.ts
@@ -0,0 +1,71 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { db, eq, users } from "@repo/database";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "johndoe@mail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        const user = (
+          await db
+            .select()
+            .from(users)
+            .where(eq(users.email, credentials.email))
+        )[0];
+
+        if (!user) {
+          return null;
+        }
+
+        const correctPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!correctPassword) {
+          return null;
+        }
+        console.log(user.firstname, user.lastname);
+        return { ...user, id: user.id.toString() };
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.sub = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user!.email = token.email;
+        session.user!.id = token.sub;
+      }
+      return session;
+    },
+  },
+};
